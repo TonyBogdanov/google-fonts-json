a3b13176c9598fde89bb4894fc8e300ae6654419
Tony Bogdanov <tonybogdanov@gmail.com>
2021-02-07 10:36:28 +0200

    fix: adapt to new design

--- src/operation/language/items/count.js
+++ src/operation/language/items/count.js
@@ -1,2 +1,2 @@
 module.exports = () => new Promise( async resolve => resolve( await browser.execute( () =>
-    document.querySelectorAll( '.script-filter-menu .menu-item' ).length ) ) );
+    document.querySelectorAll( '.script-filter-menu .mat-menu-item' ).length ) ) );
--- src/operation/language/items/first/click.js
+++ src/operation/language/items/first/click.js
@@ -1,6 +1,6 @@
 module.exports = dispatcher => new Promise( async resolve => {
 
-    ( await browser.$( '.script-filter-menu .menu-item' ) ).click();
+    ( await browser.$( '.script-filter-menu .mat-menu-item' ) ).click();
     await dispatcher.sleep( 310 );
 
     resolve( true );
